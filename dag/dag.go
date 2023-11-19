f7d17b041464dbbeea694d4bfa9e2edbb7e18320
Ece Özalp <ozale272@newschool.edu>
2023-11-19 18:03:42 -0500

    wip

--- dag/dag.go
+++ dag/dag.go
@@ -222,7 +222,139 @@ func (c *Context) AddToLayers() {
 }
 
 func (c *Context) OptimizeRowOrder() {
-	return
+	computeDownwardClosure := func() {
+		// y -2 because the very last layer is edges with nothing downward. and the layer before that are connectors ( I think)
+		for y := len(c.Layers) - 2; y > 0; y-- {
+			currentLayer := &c.Layers[y]
+			for _, upwardNodeId := range currentLayer.NodeIds {
+				upwardNode := &c.Nodes[upwardNodeId]
+				for downwardNodeId := range upwardNode.DownwardNodeIds {
+					upwardNode.DownwardClosure[downwardNodeId] = true
+					for nodeId := range c.Nodes[downwardNodeId].DownwardClosure {
+						upwardNode.DownwardClosure[nodeId] = true
+					}
+				}
+			}
+		}
+	}
+
+	computeDownwardDistances := func(layerId int, layerWidth int) {
+		// create matrix
+		distanceMatrix := make([][]int, layerWidth)
+		for i := range distanceMatrix {
+			distanceMatrix[i] = make([]int, layerWidth)
+		}
+
+		// calculate matrix values
+		for a := 0; a < layerWidth; a++ {
+			for b := 0; b < layerWidth; b++ {
+				nodeA := &c.Nodes[c.Layers[layerId].NodeIds[a]]
+				nodeB := &c.Nodes[c.Layers[layerId].NodeIds[b]]
+				commonDownwardNodeIds := make([]int, 0)
+				for downwardNodeIdForA := range nodeA.DownwardClosure {
+					if nodeB.DownwardClosure[downwardNodeIdForA] {
+						commonDownwardNodeIds = append(commonDownwardNodeIds, downwardNodeIdForA)
+					}
+				}
+				for _, commonDownwardNodeId := range commonDownwardNodeIds {
+					d := distanceMatrix[a][b]
+					distanceMatrix[a][b] = min(d, c.Nodes[commonDownwardNodeId].Layer-c.Nodes[a].Layer)
+				}
+			}
+		}
+
+	}
+
+	computeDownwardClosure()
+	for layerId, layer := range c.Layers {
+		computeDownwardDistances(layerId, len(layer.NodeIds))
+
+	}
+
+	//for _, layer := range c.layers {
+	//	layerWidth := len(layer.nodes)
+	//
+	//	// Compute inter-node downward distances.
+	//	distances := make([][]int, layerWidth)
+	//	for i := range distances {
+	//		distances[i] = make([]int, layerWidth)
+	//	}
+	//	for a := 0; a < layerWidth; a++ {
+	//		for b := 0; b < layerWidth; b++ {
+	//			nodeA := &c.nodes[layer.nodes[a]]
+	//			nodeB := &c.nodes[layer.nodes[b]]
+	//			intersection := make([]int, 0)
+	//			for ancestor := range nodeA.downwardClosure {
+	//				if nodeB.downwardClosure[ancestor] {
+	//					intersection = append(intersection, ancestor)
+	//				}
+	//			}
+	//			for _, ancestor := range intersection {
+	//				d := distances[a][b]
+	//				distances[a][b] = min(d, c.nodes[ancestor].Layer-c.nodes[layer.nodes[a]].Layer)
+	//			}
+	//		}
+	//	}
+	//
+	//	parentMean := make([]float64, layerWidth)
+	//	for a := 0; a < layerWidth; a++ {
+	//		d := 0.0
+	//		nodeDown := &c.nodes[layer.nodes[a]]
+	//		for _, b := range nodeDown.Upward {
+	//			nodeUp := &c.nodes[b]
+	//			d += float64(nodeUp.Row)
+	//		}
+	//		parentMean[a] = d / (float64(len(nodeDown.Upward)) + 0.01)
+	//	}
+	//
+	//	permutation := make([]int, layerWidth)
+	//	for i := 0; i < layerWidth; i++ {
+	//		permutation[i] = i
+	//	}
+	//
+	//	evaluateScore := func() float64 {
+	//		score := 0.0
+	//		for i := 0; i < layerWidth-1; i++ {
+	//			score += float64(distances[permutation[i]][permutation[i+1]])
+	//		}
+	//		for i := 0; i < layerWidth; i++ {
+	//			d := float64(i) - parentMean[permutation[i]]
+	//			score += d * d * 15
+	//		}
+	//		return score
+	//	}
+	//
+	//	score := evaluateScore()
+	//	scoreLastLoop := 0.0
+	//	for score != scoreLastLoop {
+	//		scoreLastLoop = score
+	//		for a := 0; a < layerWidth; a++ {
+	//			for b := 0; b < layerWidth; b++ {
+	//				permutation[a], permutation[b] = permutation[b], permutation[a]
+	//				newScore := evaluateScore()
+	//				if newScore < score {
+	//					score = newScore
+	//				} else {
+	//					permutation[a], permutation[b] = permutation[b], permutation[a]
+	//				}
+	//			}
+	//		}
+	//	}
+	//
+	//	// Reorder the nodes inside the layer.
+	//	orderedNodes := make([]int, layerWidth)
+	//	for i, p := range permutation {
+	//		orderedNodes[i] = layer.nodes[p]
+	//	}
+	//	layer.nodes = orderedNodes
+	//
+	//	// Precompute every node's row.
+	//	for i := 0; i < layerWidth; i++ {
+	//		node := &c.nodes[layer.nodes[i]]
+	//		node.Row = i
+	//	}
+	//}
+
 }
 
 func (c *Context) ResolveCrossingEdges() {
